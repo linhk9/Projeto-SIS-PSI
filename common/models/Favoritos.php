1d408dbd419751451d70b75e536c6425dce41ddd
Rodrigo Luís <54328638+linhk9@users.noreply.github.com>
2024-01-12 20:13:11 +0000

    Mosquitto

--- common/models/Favoritos.php
+++ common/models/Favoritos.php
@@ -2,6 +2,7 @@
 
 namespace common\models;
 
+use app\mosquitto\phpMQTT;
 use Yii;
 
 /**
@@ -66,5 +67,32 @@ class Favoritos extends \yii\db\ActiveRecord
     public function getUserdata()
     {
         return $this->hasOne(Userdata::class, ['id' => 'id_userdata']);
+    }
+
+    public function afterSave($insert, $changedAttributes)
+    {
+        parent::afterSave($insert, $changedAttributes);
+        $myObj=new \stdClass();
+        $myObj->mensagem='ALERTA: Novo produto adicionado aos favoritos';
+        $myJSON = json_encode($myObj);
+
+        $this->FazPublishNoMosquitto("favoritos",$myObj);
+
+    }
+
+    public function FazPublishNoMosquitto($canal,$msg)
+    {
+        $server = "127.0.0.1";
+        $port = 1883;
+        $username = "";
+        $password = "";
+        $client_id = "phpMQTT-publisher";
+        $mqtt = new phpMQTT($server, $port, $client_id);
+        if ($mqtt->connect(true, NULL, $username, $password)) {
+            $mqtt->publish($canal, $msg, 0);
+            $mqtt->close();
+        } else {
+            file_put_contents("debug.output", "Time out!");
+        }
     }
 }
